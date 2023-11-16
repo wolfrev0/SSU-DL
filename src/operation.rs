89334e5860bfedf3d7177df677ee1c0acf7e8dc8
tuxedcat <tuxedcat@gmail.com>
2023-11-17 08:48:55 +0900

    smce batch

--- src/operation.rs
+++ src/operation.rs
@@ -1,4 +1,4 @@
-use ndarray::{s, Array4, Axis};
+use ndarray::{s, Array3, Array4, Axis};
 
 //fwd와 bwd 설명
 //fwd(input_1, input_2, ..., input_N) -> output
@@ -36,8 +36,8 @@ fn bfyx_matmul(a: &Array4<f32>, b: &Array4<f32>) -> Array4<f32> {
 	let (b1, f1, y1, x1) = (b.shape()[0], b.shape()[1], b.shape()[2], b.shape()[3]);
 	assert!(b0 == b1 && f0 == f1 && x0 == y1);
 	let mut ret = Array4::zeros((a.shape()[0], a.shape()[1], a.shape()[2], b.shape()[3]));
-	for i in 0..a.shape()[0] {
-		for j in 0..a.shape()[1] {
+	for i in 0..b0 {
+		for j in 0..f0 {
 			let cur = a.slice(s![i, j, .., ..]).dot(&b.slice(s![i, j, .., ..]));
 			// Update the result array
 			ret.slice_mut(s![i, j, .., ..]).assign(&cur);
@@ -138,14 +138,31 @@ pub fn cross_entropy_back(input: &Vec<Array4<f32>>) -> Vec<Array4<f32>> {
 //input[1]=truth
 //output[0]=output
 pub fn softmax_cross_entropy(input: &Vec<Array4<f32>>) -> Array4<f32> {
-	//TODO: support b>1 f>1 case
-	let ret = input[0].map(|x| x.exp());
-	let denom = ret.sum();
-	let softmax_out = ret / denom;
-	Array4::from_elem(
-		(1, 1, 1, 1),
-		(softmax_out.map(|x| -x.ln()) * &input[1]).sum(),
-	)
+	let (b0, f0, y0, x0) = (
+		input[0].shape()[0],
+		input[0].shape()[1],
+		input[0].shape()[2],
+		input[0].shape()[3],
+	);
+	let (b1, f1, y1, x1) = (
+		input[1].shape()[0],
+		input[1].shape()[1],
+		input[1].shape()[2],
+		input[1].shape()[3],
+	);
+	assert!(b0 == b1 && f0 == f1 && y0 == y1 && x0 == x1);
+	let mut ret = Array4::zeros((b0, 1, 1, 1));
+	for i in 0..b0 {
+		let m0exp = input[0].slice(s![i, .., .., ..]).map(|x| x.exp());
+		let m1 = input[1].slice(s![i, .., .., ..]);
+		let denom = m0exp.sum();
+		let softmax_out = m0exp / denom;
+		ret.slice_mut(s![i, .., .., ..]).assign(&Array3::from_elem(
+			(1, 1, 1),
+			(softmax_out.map(|x| -x.ln()) * &m1).sum(),
+		));
+	}
+	ret
 }
 
 //input[-1]=output_grad_sum
@@ -154,12 +171,32 @@ pub fn softmax_cross_entropy(input: &Vec<Array4<f32>>) -> Array4<f32> {
 //output[0]=input_grad
 //output[0]=truth_grad
 pub fn softmax_cross_entropy_back(input: &Vec<Array4<f32>>) -> Vec<Array4<f32>> {
-	//TODO: support b>1 f>1 case
-	let ret = input[0].map(|x| x.exp());
-	let denom = ret.sum();
-	let softmax_out = ret / denom;
-	vec![
-		(softmax_out - &input[1]) * &input[2],
-		input[0].map(|x| -x.ln()) * &input[2],
-	]
+	let (b0, f0, y0, x0) = (
+		input[0].shape()[0],
+		input[0].shape()[1],
+		input[0].shape()[2],
+		input[0].shape()[3],
+	);
+	let (b1, f1, y1, x1) = (
+		input[1].shape()[0],
+		input[1].shape()[1],
+		input[1].shape()[2],
+		input[1].shape()[3],
+	);
+	let (b2, f2, y2, x2) = (1, 1, 1, 1);
+	assert!(b0 == b1 && f0 == f1 && y0 == y1 && x0 == x1);
+	let mut ret0 = Array4::zeros((b0, 1, y1, x2));
+	let mut ret1 = Array4::zeros((b0, 1, y0, x2));
+	for i in 0..b0 {
+		let m0 = input[0].slice(s![i, .., .., ..]);
+		let m1 = input[1].slice(s![i, .., .., ..]);
+		let m2 = input[2].slice(s![i, .., .., ..]);
+		let denom = m0.map(|x| x.exp()).sum();
+		let softmax_out = m0.map(|x| x.exp()) / denom;
+		ret0.slice_mut(s![i, .., .., ..])
+			.assign(&((softmax_out - &m1) * &m2));
+		ret1.slice_mut(s![i, .., .., ..])
+			.assign(&(m0.map(|x| -x.ln()) * &m2));
+	}
+	vec![ret0, ret1]
 }
